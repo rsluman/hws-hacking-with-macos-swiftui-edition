11620e31be5ce8db1a9259d00df701ca6b206371
Roelf Sluman <roelf@iosacademie.nl>
2024-07-27 16:38:59 +0200

    Project 2 - Cows and Bulls: Initial Commit

---
+++ CowsAndBulls/CowsAndBulls/CowsAndBullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsApp.swift
+//  CowsAndBulls
+//
+//  Created by Roelf Sluman on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

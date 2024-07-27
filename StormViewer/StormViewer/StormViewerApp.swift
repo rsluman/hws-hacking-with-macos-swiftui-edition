4fe47485e25de18e0ad612004f310f02caddb52a
Roelf Sluman <roelf@iosacademie.nl>
2024-07-27 15:14:24 +0200

    Storm Viewer: Setting up

---
+++ StormViewer/StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Roelf Sluman on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

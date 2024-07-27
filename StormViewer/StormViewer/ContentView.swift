baaa9e1b2217627d752aaec0305bc0541459fa63
Roelf Sluman <roelf@iosacademie.nl>
2024-07-27 16:36:41 +0200

    Project 1 - Storm Viewer - Final version

--- StormViewer/StormViewer/ContentView.swift
+++ StormViewer/StormViewer/ContentView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var selectedImage: Int?
+  
+  var body: some View {
+    NavigationSplitView {
+      List(0..<10, selection: $selectedImage) { number in
+        Text("Storm \(number + 1)")
+      }
+      .frame(width: 150)
+    } detail: {
+      if let selectedImage {
+        Image("\(selectedImage)")
+          .resizable()
+          .scaledToFit()
+      } else {
+        Text("Please select an image")
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+      }
     }
+    .frame(minWidth: 480, minHeight: 320)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
--- StormViewer/StormViewer/StormViewerApp.swift
+++ StormViewer/StormViewer/StormViewerApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct StormViewerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+  var body: some Scene {
+    Window("Storm Viewer", id: "main") {
+      ContentView()
+        .onAppear() {
+          NSWindow.allowsAutomaticWindowTabbing = false
         }
     }
+    
+    .commands {
+      CommandGroup(replacing: .newItem) { }
+      CommandGroup(replacing: .undoRedo) { }
+      CommandGroup(replacing: .pasteboard) { }
+    }
+  }
 }

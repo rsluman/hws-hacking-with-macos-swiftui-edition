a252e04005c47a3e3b69ca650f769a38c365d68c
Roelf Sluman <roelf@iosacademie.nl>
2024-07-27 16:56:47 +0200

    Project 2 - Cows and Bulls: Designing our user interface

--- CowsAndBulls/CowsAndBulls/ContentView.swift
+++ CowsAndBulls/CowsAndBulls/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  let guesses = Array(repeating: "1234", count: 20)
+  
+  var body: some View {
+    VStack(spacing: 0) {
+      HStack {
+        TextField("Enter a guess…", text: .constant("1234"))
+        Button("Go", action: submitGuess)
+      }
+      .padding()
+      
+      List(guesses, id: \.self) { guess in
+        HStack {
+          Text(guess)
+          Spacer()
+          Text("4c 0b")
         }
-        .padding()
+      }
     }
+    .frame(width: 250)
+    .frame(minHeight: 300)
+  }
+  
+  func submitGuess() {
+    
+  }
+  
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
--- CowsAndBulls/CowsAndBulls/CowsAndBullsApp.swift
+++ CowsAndBulls/CowsAndBulls/CowsAndBullsApp.swift
@@ -13,5 +13,6 @@ struct CowsAndBullsApp: App {
         WindowGroup {
             ContentView()
         }
+        .windowResizability(.contentSize)
     }
 }

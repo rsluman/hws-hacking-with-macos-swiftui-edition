cdff16ed60d857bc85c9d404daa0f22df432be73
Roelf Sluman <roelf@iosacademie.nl>
2024-07-28 13:45:18 +0200

    Project 2 - Cows and Bulls: Marking guesses

--- CowsAndBulls/CowsAndBulls/ContentView.swift
+++ CowsAndBulls/CowsAndBulls/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
   @State private var guesses = [String]()
   @State private var guess = ""
   @State private var answer = ""
+  @State private var isGameOver = false
   
   var body: some View {
     VStack(spacing: 0) {
@@ -37,25 +38,52 @@ struct ContentView: View {
     .onAppear() {
       startNewGame()
     }
+    
+    .alert("You win!", isPresented: $isGameOver) {
+      Button("OK", action: startNewGame)
+    } message: {
+      Text("Congratulations: click OK to play again.")
+    }
+    
   }
   
   func submitGuess() {
-    if guess.count == answerLength {
-      guesses.append(guess)
+    guard guess.count == answerLength else { return } // four numbers
+    guard Set(guess).count == answerLength else { return } // four *unique* numbers
+    
+    let badCharacters = CharacterSet.decimalDigits.inverted
+    guard guess.rangeOfCharacter(from: badCharacters) == nil else { return }
+    
+    guesses.insert(guess, at: 0)
+    
+    // Check for win
+    if result(for: guess).contains("\(answerLength)b") {
+      isGameOver = true
+    } else {
       guess = ""
     }
     
   }
   
   func result(for guess: String) -> String {
-    return "Result"
+    var bulls = 0
+    var cows = 0
+    
+    for (position, number) in Array(guess).enumerated() {
+      if number == Array(answer)[position] {
+        bulls += 1
+      } else if Array(answer).contains(number) {
+        cows += 1
+      }
+    }
+    
+    return "\(cows)c \(bulls)b"
   }
   
   func startNewGame() {
     guess = ""
     guesses.removeAll()
     createAnswer()
-    
   }
   
   func createAnswer() {

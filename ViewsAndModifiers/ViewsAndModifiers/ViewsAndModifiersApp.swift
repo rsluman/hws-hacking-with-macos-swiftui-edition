3f3689d499904f034102151f1a658f5e9261fe25
Roelf Sluman <roelf@iosacademie.nl>
2024-08-11 10:11:41 +0200

    Project 3 - Views and modifiers: Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Roelf Sluman on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

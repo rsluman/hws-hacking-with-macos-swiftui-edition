0d3d4feb055d266d9fc3583e33e4bc80094c6232
Roelf Sluman <roelf@iosacademie.nl>
2024-08-11 10:45:59 +0200

    Project 3 - Views and modifiers: regular vs environment modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Text("Gryffindor")
+        .blur(radius: 3) // adds to regular modifier
+        .font(.largeTitle) // overrules environment modifier
+      Text("Hufflepuff")
+      Text("Ravenclaw")
+      Text("Slytherin")
     }
+    .blur(radius: 1) // regular modifier
+    .font(.title) // environment modifier
+    
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }

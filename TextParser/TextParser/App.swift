82e08d57ce840546d529d5493c16974940bf18db
Roelf Sluman <roelf@iosacademie.nl>
2024-08-11 11:25:00 +0200

    Project 4 - Text Parser: Performing semantic analysis

--- TextParser/TextParser/App.swift
+++ TextParser/TextParser/App.swift
@@ -6,12 +6,25 @@
 //
 
 import Foundation
+import NaturalLanguage
 
 @main
 struct App {
+  static func sentiment(for string: String) -> Double {
+    let tagger = NLTagger(tagSchemes: [.sentimentScore])
+    tagger.string = string
+    let (sentiment, _) = tagger.tag(at: string.startIndex, unit: .paragraph, scheme: .sentimentScore)
+    guard let sentiment else { return 0 }
+    
+    let result = Double(sentiment.rawValue) ?? 0
+    
+    return result
+  }
+  
   static func main() {
     let text = CommandLine.arguments.dropFirst().joined(separator: " ")
-    print(text)
+    let sentiment = sentiment(for: text)
+    print("Sentiment for '\(text)': \(sentiment)")
   }
 }
 

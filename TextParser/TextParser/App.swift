dcb949eced20470bec1baea1e1ea95e6575e7f8b
Roelf Sluman <roelf@iosacademie.nl>
2024-08-11 12:19:24 +0200

    Project 4 - Text Parser: Finding alternative words

--- TextParser/TextParser/App.swift
+++ TextParser/TextParser/App.swift
@@ -21,10 +21,49 @@ struct App {
     return result
   }
   
+  static func embeddings(for word: String) -> [String] {
+    var results = [String]()
+    
+    if let embedding = NLEmbedding.wordEmbedding(for: .english) {
+      let similarWords = embedding.neighbors(for: word, maximumCount: 10)
+      for word in similarWords {
+        results.append("\(word.0) has a distance of \(word.1)")
+      }
+    }
+    
+    return results
+  }
+  
+  static func lemmatize(string: String) -> [String] {
+    let tagger = NLTagger(tagSchemes: [.lemma])
+    tagger.string = string
+    
+    var results = [String]()
+    
+    tagger.enumerateTags(in: string.startIndex..<string.endIndex, unit: .word, scheme: .lemma) { tag, range in
+      let stemForm = tag?.rawValue ?? String(string[range]).trimmingCharacters(in: .whitespaces)
+      if !stemForm.isEmpty  {
+        results.append(stemForm)
+      }
+      return true
+    }
+    
+    return results
+  }
+  
   static func main() {
     let text = CommandLine.arguments.dropFirst().joined(separator: " ")
     let sentiment = sentiment(for: text)
     print("Sentiment for '\(text)': \(sentiment)")
+    print()
+    print("Found the following alternatives:")
+    
+    let lemma = lemmatize(string: text)
+    
+    for word in lemma {
+      let embeddings = embeddings(for: word)
+      print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+    }
   }
 }
 

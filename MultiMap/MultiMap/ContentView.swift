fd22d2b168e462a35bebac9340ca7b665fbc434e
Roelf Sluman <roelf@iosacademie.nl>
2024-08-17 10:30:37 +0200

    Project 5 - MultiMap: Creating an interactive map - initial map (London)

--- MultiMap/MultiMap/ContentView.swift
+++ MultiMap/MultiMap/ContentView.swift
@@ -5,20 +5,21 @@
 //  Created by Roelf Sluman on 17/08/2024.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+  @State private var mapCamera = MapCameraPosition.region(
+    MKCoordinateRegion(
+      center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+      span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+    ))
+
+  var body: some View {
+    Map(position: $mapCamera)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }

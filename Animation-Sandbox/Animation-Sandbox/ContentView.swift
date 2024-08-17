7799ec26e198c558bb6be99b5a6c68d5fff0441c
Roelf Sluman <58142572+rsluman@users.noreply.github.com>
2024-08-17 13:50:02 +0200

    Project 6 - Animation: Creating implicit animations

--- Animation-Sandbox/Animation-Sandbox/ContentView.swift
+++ Animation-Sandbox/Animation-Sandbox/ContentView.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+  @State private var animationAmount = 1.0
+  
+  var body: some View {
+    Text("Click Me")
+      .onTapGesture {
+        animationAmount += 1
+      }
+      .padding(50)
+      .background(.red)
+      .foregroundStyle(.white)
+      .clipShape(.circle)
+      .padding(100)
+      .scaleEffect(animationAmount)
+      .blur(radius: (animationAmount - 1) * 3)
+      .animation(.default, value: animationAmount)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }

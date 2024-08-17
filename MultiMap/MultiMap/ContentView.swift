ce6c9e67d8ef7de227c42c7749af767449d1348b
Roelf Sluman <roelf@iosacademie.nl>
2024-08-17 10:47:27 +0200

    Project 5 - MultiMap: Creating an interactive map - adding annotations

--- MultiMap/MultiMap/ContentView.swift
+++ MultiMap/MultiMap/ContentView.swift
@@ -15,8 +15,26 @@ struct ContentView: View {
       span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
     ))
 
+  let locations =
+  [Location(name: "London", latitude: 51.507222, longitude: -0.1275),
+   Location(name: "Glasgow", latitude: 55.8616752, longitude: -4.2546099)
+   ]
+
   var body: some View {
-    Map(position: $mapCamera)
+    Map(position: $mapCamera) {
+      ForEach(locations) { location in
+        Annotation(location.name, coordinate: location.coordinate) {
+          Text(location.name)
+            .font(.headline)
+            .padding(5)
+            .padding(.horizontal, 5)
+            .background(.black)
+            .foregroundStyle(.white)
+            .clipShape(.capsule)
+        }
+      }
+    }
+    
   }
 }
 
---
+++ MultiMap/MultiMap/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  MultiMap
+//
+//  Created by Roelf Sluman on 17/08/2024.
+//
+
+import MapKit
+
+struct Location: Hashable, Identifiable {
+  let id = UUID()
+  let name: String
+  let latitude: Double
+  let longitude: Double
+  
+  var coordinate: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+  }
+}

2c9bd9e283a34ec1f280bca6cf872a75467195f5
Roelf Sluman <roelf@iosacademie.nl>
2024-08-17 10:22:48 +0200

    Project 5 - MultiMap: Introduction (Initial Commit)

---
+++ MultiMap/MultiMap/MultiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by Roelf Sluman on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

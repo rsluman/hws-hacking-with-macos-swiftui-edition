7059eb2cddd3c9dc42419b5075ad905e0ea25fa0
Roelf Sluman <58142572+rsluman@users.noreply.github.com>
2024-08-17 13:41:32 +0200

    Project 6 - Animation: Initial Commit

---
+++ Animation-Sandbox/Animation-Sandbox/Animation_SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_SandboxApp.swift
+//  Animation-Sandbox
+//
+//  Created by Roelf Sluman on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Animation_SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
